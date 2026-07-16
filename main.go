4bb2f8f43d8bb461b67145f98d016d2bcb037f14
Sunr1se <david_gosha@knu.ua>
2026-07-16 10:43:35 +0200

    done

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // TODO (what-is-zk): implement per the lesson description.
@@ -14,11 +15,15 @@ func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
 	for sc.Scan() {
-		line := sc.Text()
-		if line == "" {
+		parts := strings.Fields(sc.Text())
+		if len(parts) == 0 {
 			continue
 		}
-		n, err := strconv.Atoi(line)
+		if parts[0] != "ROUNDS" || len(parts) != 2 {
+			continue
+		}
+
+		n, err := strconv.Atoi(parts[1])
 		if err != nil {
 			panic(err)
 		}
