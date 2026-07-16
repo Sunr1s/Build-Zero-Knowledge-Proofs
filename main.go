9a211bfc9d0eef0124b370a45f206b2ace739fe1
Sunr1se <david_gosha@knu.ua>
2026-07-16 12:17:42 +0200

    implement Shorr challenge

--- main.go
+++ main.go
@@ -2,40 +2,41 @@ package main
 
 import (
 	"bufio"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
+	"math/big"
 	"os"
 	"strings"
 )
-
-// TODO (interactive-non-interactive): implement per the lesson description.
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
 
 	for sc.Scan() {
-
 		parts := strings.Fields(sc.Text())
-		if len(parts) != 3 || parts[0] != "PROOF" {
+		if len(parts) != 7 || parts[0] != "VERIFY" {
 			continue
 		}
 
-		commitmentBytes, err := hex.DecodeString(parts[1])
-		if err != nil {
+		generator, ok1 := new(big.Int).SetString(parts[1], 10)
+		primeModulus, ok2 := new(big.Int).SetString(parts[2], 10)
+		publicValue, ok3 := new(big.Int).SetString(parts[3], 10)
+		commitment, ok4 := new(big.Int).SetString(parts[4], 10)
+		challenge, ok5 := new(big.Int).SetString(parts[5], 10)
+		response, ok6 := new(big.Int).SetString(parts[6], 10)
+		if !(ok1 && ok2 && ok3 && ok4 && ok5 && ok6) {
 			continue
 		}
 
-		statementBytes, err := hex.DecodeString(parts[2])
-		if err != nil {
-			continue
+		left := new(big.Int).Exp(generator, response, primeModulus)
+		publicPart := new(big.Int).Exp(publicValue, challenge, primeModulus)
+		right := new(big.Int).Mul(commitment, publicPart)
+		right.Mod(right, primeModulus)
+
+		if left.Cmp(right) == 0 {
+			fmt.Println("OK")
+		} else {
+			fmt.Println("BAD")
 		}
-
-		concatBytes := append(commitmentBytes, statementBytes...)
-		hash := sha256.Sum256(concatBytes)
-
-		result := hex.EncodeToString(hash[:16])
-		fmt.Println(result)
 	}
 }
