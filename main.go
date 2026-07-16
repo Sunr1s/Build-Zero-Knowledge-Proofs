18f897056e95e93a2f5e27a0ba85b73d1dc09bce
Sunr1se <david_gosha@knu.ua>
2026-07-16 10:42:21 +0200

    done

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
 // TODO (what-is-zk): implement per the lesson description.
@@ -13,7 +15,15 @@ func main() {
 	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
 	for sc.Scan() {
 		line := sc.Text()
-		if line == "" { continue }
-		fmt.Println("TODO")
+		if line == "" {
+			continue
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+
+		b := math.Pow(0.5, float64(n))
+		fmt.Printf("cheat_probability=%f\n", b)
 	}
 }
